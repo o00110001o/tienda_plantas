bdfca4cd5a62e7b201bb7ff0bd83f27c2a1adc98
o00110001o <45133321+o00110001o@users.noreply.github.com>
2022-07-13 06:33:50 -0400

    Add files via upload

---
+++ app/static/js/api.js
@@ -0,0 +1,13 @@
+
+
+let url = "https://rickandmortyapi.com/"
+$.get(url, function(respuesta){
+    
+    respuesta.forEach(function (tem) {
+
+    let api = respuesta[respuesta]
+    $("#rick").text(character.name)
+        
+    })
+
+} , "json")
