d147e4703b9c7659dbaa9cfeca7c1cb1751d2960
o00110001o <45133321+o00110001o@users.noreply.github.com>
2022-07-13 06:29:54 -0400

    Add files via upload

--- apiapp/serializers.py
+++ apiapp/serializers.py
@@ -2,8 +2,6 @@
 
 from app.models import *
 from rest_framework import serializers
-from rest_framework import serializers
-
 
 class TipoProductoSerializer(serializers.ModelSerializer): #el serializador hace el crud
     class Meta:
@@ -18,4 +16,4 @@ class ProductoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Producto
-        fields = '__all__' #sirve para cargar todos los campos que tiene el modelo. NO SE USA EN REGISTRO DE USER
+        fields = '__all__' 
--- apiapp/urls.py
+++ apiapp/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import *
 
-router = routers.DefaultRouter()
-router.register('producto', ProductoViewSet)
+router = routers.DefaultRouter() #Nos entrega el enrutador por defecto/ ahora se puede modificar
+router.register('producto', ProductoViewSet) # esta con la de arriba generan el get y el post
 
 app_name = 'apiapp'
 urlpatterns = [
